d0410d5834ff1a359d108dccafaac18d19db8446
Harizibam7 <harizibamv@gmail.com>
2024-12-08 12:00:06 +0530

    Modified  DashboardComp

--- frontend/src/components/DashJobs.jsx
+++ frontend/src/components/DashJobs.jsx
@@ -66,6 +66,7 @@ export default function DashJobs() {
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentstudent.isAdmin && totalJobs.length > 0 ? (
         <>
+        
           <Table hoverable className='shadow-md'>
             <Table.Head>
               <Table.HeadCell>Company</Table.HeadCell>
